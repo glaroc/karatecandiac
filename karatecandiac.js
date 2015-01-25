07548b81f1c81a9211c2a1b84fc2cf28c7114d77
glaroc <glaroc@gmail.com>
2015-01-25 11:45:41 -0500

    first commit

---
+++ karatecandiac.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#kata1vid').click(function(){
+		var id = "MGLl88TYl5w";
+		var ref = window.open('http://www.youtube.com/embed/'+id+'?html5=1', '_blank', 'location=yes');
+	});
+	$('#kata3vid').click(function(){
+		var id = "paWbNEikEMs";
+		var ref = window.open('http://www.youtube.com/embed/'+id+'?html5=1', '_blank', 'location=yes');
+	});
+	$('#kata3vid').click(function(){
+		var id = "I0nNKv1i3sw";
+		var ref = window.open('http://www.youtube.com/embed/'+id+'?html5=1', '_blank', 'location=yes');
+	});
+});
