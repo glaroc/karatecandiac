81bc440132bd86ed38208defe9395320e711b587
glaroc <glaroc@gmail.com>
2015-01-25 14:08:59 -0500

    logo

--- karatecandiac.js
+++ karatecandiac.js
@@ -1,14 +1,7 @@
 $(document).ready(function(){
-	$('#kata1vid').click(function(){
+	$('.vid').click(function(){
+		var id= $(this).attr("vid");
 		var id = "MGLl88TYl5w";
 		var ref = window.open('http://www.youtube.com/embed/'+id+'?html5=1', '_blank', 'location=yes');
 	});
-	$('#kata3vid').click(function(){
-		var id = "paWbNEikEMs";
-		var ref = window.open('http://www.youtube.com/embed/'+id+'?html5=1', '_blank', 'location=yes');
-	});
-	$('#kata3vid').click(function(){
-		var id = "I0nNKv1i3sw";
-		var ref = window.open('http://www.youtube.com/embed/'+id+'?html5=1', '_blank', 'location=yes');
-	});
-});
+}
