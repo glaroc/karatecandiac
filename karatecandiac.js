b1768aea24ddb94e9e2f20d4de20e8d57abf76bb
glaroc <glaroc@gmail.com>
2015-01-25 17:53:53 -0500

    debug pdfs

--- karatecandiac.js
+++ karatecandiac.js
@@ -9,24 +9,28 @@ $(document).ready(function(){
 	});
 	$('.techmenu li').click(function(){
 		var ref = $(this).find('a').attr('tar');
-		//href=$(this).find('a').attr('tar');
-		//window.plugins.fileOpener.open(href);
-		var fileTransfer = new FileTransfer();
-		var uri = encodeURI(ref);
-
-		fileTransfer.download(
-		    uri,
-		    "pdfs/",
-		    function(entry) {
-		    },
-		    function(error) {
-		    },
-		    false,
-		    {
-		        headers: {
-		        }
-		    }
-		);
+		download(ref);
 	});
 
 });
+
+
+
+
+function download(file) {
+    var remoteFile = file;
+    var localFileName = remoteFile.substring(remoteFile.lastIndexOf('/')+1);
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+        fileSystem.root.getFile(localFileName, {create: true, exclusive: false}, function(fileEntry) {
+            var localPath = fileEntry.fullPath;
+            if (device.platform === "Android" && localPath.indexOf("file://") === 0) {
+                localPath = localPath.substring(7);
+            }
+            var ft = new FileTransfer();
+            ft.download(remoteFile,
+                localPath, function(entry) {
+                    window.plugins.fileOpener.open(entry.fullPath);
+                }, fail);
+        }, fail);
+    }, fail);
+}
