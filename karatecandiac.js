971481a4226cc38730766b023412f992e1dc5c2b
Guillaume Larocque <glaroc@gmail.com>
2015-01-26 12:34:25 -0500

    debug pdf

--- karatecandiac.js
+++ karatecandiac.js
@@ -4,26 +4,23 @@ function load(){
 }
 
 function onDeviceReady() {
+    $('.videomenu li').click(function(){
+        var id= $(this).find('a').attr("vid");
+        if (parseInt(id.substring(0,1))>0) {
+            var ref = window.open('http://player.vimeo.com/video/'+id, '_blank', 'location=yes');
+        }else{
+            var ref = window.open('http://www.youtube.com/embed/'+id+'?html5=1', '_blank', 'location=yes')
+        }
+    });
+
+    $('.mainmenu li').click(function(){
+        var ref = window.open($(this).find('a').attr('tar'));
+    });
      $('.techmenu li').click(function(){
             var ref = $(this).find('a').attr('tar');
             downloadFile(ref);
      });
- }
-
-$(document).ready(function(){
-	$('.videomenu li').click(function(){
-		var id= $(this).find('a').attr("vid");
-		if (parseInt(id.substring(0,1))>0) {
-			var ref = window.open('http://player.vimeo.com/video/'+id, '_blank', 'location=yes');
-		}else{
-			var ref = window.open('http://www.youtube.com/embed/'+id+'?html5=1', '_blank', 'location=yes')
-		}
-	});
-
-	$('.mainmenu li').click(function(){
-		var ref = window.open($(this).find('a').attr('tar'));
-	});
-});
+}
 
 function downloadFile(ref) {
 	$('#ready').html(ref);
