281269b03084a760f09083d3edca910f10e99e2c
glaroc <glaroc@gmail.com>
2015-01-25 20:00:04 -0500

    logo

--- karatecandiac.js
+++ karatecandiac.js
@@ -38,3 +38,35 @@ function download(file) {
         }, fail);
     }, fail);
 }
+
+
+function downloadFile(filename){
+    window.requestFileSystem(
+                 LocalFileSystem.PERSISTENT, 0, 
+                 function onFileSystemSuccess(fileSystem) {
+                 fileSystem.root.getFile(
+                             "dummy.html", {create: true, exclusive: false}, 
+                             function gotFileEntry(fileEntry){
+                             var sPath = fileEntry.fullPath.replace("dummy.html","");
+                             var fileTransfer = new FileTransfer();
+                             fileEntry.remove();
+
+                             fileTransfer.download(
+                                       filename,
+                                       sPath + "theFile.pdf",
+                                       function(theFile) {
+                                       console.log("download complete: " + theFile.toURI());
+                                       showLink(theFile.toURI());
+                                       },
+                                       function(error) {
+                                       console.log("download error source " + error.source);
+                                       console.log("download error target " + error.target);
+                                       console.log("upload error code: " + error.code);
+                                       }
+                                       );
+                             }, 
+                             fail);
+                 }, 
+                 fail);
+
+}
